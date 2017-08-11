b5747b0dbf834bc2ff5c42e860c851b51ad91f82
Mincl <eonchan99@gmail.com>
2017-08-11 18:23:58 +0900

    main - add search form submit

--- chaeum/__init__.py
+++ chaeum/__init__.py
@@ -78,3 +78,5 @@ api.add_resource(Like, '/api/likes', '/api/likes/<int:like_id>')
 
 from chaeum.resources.web.index import Index
 api.add_resource(Index, '/index')
+from chaeum.resources.web.index import Search
+api.add_resource(Search, '/search')
