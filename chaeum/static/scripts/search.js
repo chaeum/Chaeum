2b35357d3fde9c1b52be00edfa4af32388fb0b6e
Mincl <eonchan99@gmail.com>
2017-08-13 17:07:19 +0900

    search - fix UI and js bug
    UI - modify z-index
    js - search highlight

--- chaeum/static/scripts/search.js
+++ chaeum/static/scripts/search.js
@@ -1,9 +1,9 @@
 $(function() {
-    var query = $('.content .search .text .h1').text();
+    var query = $('.content .result .text .h1').text();
     var highlight = $('<span>');
     highlight.addClass('ac2');
     highlight.text(query);
-    $('.content .search .info .name').each(function(idx) {
+    $('.content .result .info .name').each(function(idx) {
         var origin = $(this).text();
         var replaced = origin.replace(query, highlight.clone().wrap('<p>').parent().html());
         $(this).html(replaced);
