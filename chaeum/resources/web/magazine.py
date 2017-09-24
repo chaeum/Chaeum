3aa95e08f7424929bbbd8a54e2f5c6e91a78dc7a
Mincl <eonchan99@gmail.com>
2017-09-24 18:28:34 +0900

    magazine - backend logic
    pagination by curPage, pageCnt

--- chaeum/resources/web/magazine.py
+++ chaeum/resources/web/magazine.py
@@ -1,10 +1,73 @@
+from math import ceil
 from flask_restful import reqparse, Resource
+from chaeum import cnx_pool
 from chaeum.common.render import render_html
+
+list_page_parser = reqparse.RequestParser()
+list_page_parser.add_argument(
+    'page', type=int, help='current page index', default=1
+)
+
+
+def fetch_list_cnt():
+    query = """
+        SELECT COUNT(*) FROM TBMAGAZINE
+    """
+    conn = cnx_pool.get_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query)
+        result = cursor.fetchone()
+        if result is not None:
+            return result[0]
+        else:
+            return 0
+    except Exception as e:
+        print(e)
+        return 0
+
+
+def fetch_list_page(page, pageSize):
+    query = """
+        SELECT * FROM TBMAGAZINE
+        ORDER BY mod_date DESC
+        LIMIT %d, %d
+    """
+
+    conn = cnx_pool.get_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query % (page * pageSize, pageSize))
+        result = cursor.fetchall()
+        list = []
+        for res in result:
+            id, title, contents, like_cnt, comment_cnt, reg_date, mod_date = res
+            dict = {
+                "id": id,
+                "title": title,
+                "contents": contents,
+                "like_cnt": like_cnt,
+                "comment_cnt": comment_cnt,
+                "reg_date": reg_date,
+                "mod_date": mod_date
+            }
+            list.append(dict)
+        return list
+    except Exception as e:
+        print('fetch_list_page- %s' % e)
+        return []
 
 
 class WebMagazineList(Resource):
     def get(self):
-        return render_html('magazine/magazine.html')
+        args = list_page_parser.parse_args()
+
+        pageSize = 4
+        listSize = fetch_list_cnt()
+        pageCnt = ceil((listSize-1) / pageSize)
+        list = fetch_list_page(args.page-1, pageSize)
+
+        return render_html('magazine/magazine.html', list=list, page=args.page, pageCnt=pageCnt)
 
 
 # class WebMagazineDetail(Resource):
